957a8bbd29f314b1c04c717f137062e27976b83a
aredj <aredja2@gmail.com>
2025-12-30 12:30:14 -0700

    Lacayto Update

--- js/navigation.js
+++ js/navigation.js
@@ -13,6 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
                         <a href="house-plan-chynoweth.html">The Chynoweth Plan</a>
                         <a href="house-plan-mehew.html">The Mehew Plan</a>
                         <a href="house-plan-pinales.html">The Pinales Plan</a>
+                        <a href="house-plan-pinales.html">The Pinales Plan</a>
+                        <a href="house-plan-lacayo.html">The Lacayo Plan</a>
                         <a href="house-plan-custom.html">Custom Plans</a>
                     </div>
                 </li>
